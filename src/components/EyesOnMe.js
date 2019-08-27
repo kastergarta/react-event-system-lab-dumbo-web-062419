6f0f95748e9932fde39a1437d629bd302652280f
Dennis Basyrov <dennis.basyrov@gmail.com>
2019-08-27 16:41:14 -0400

    100% Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+
+onBlur = () => console.log('Hey! Eyes on me!')
+
+onFocus = () => console.log('Good!')
+
+render(){
+
+    return(
+      <button onFocus={this.onFocus} onBlur={this.onBlur}>Enter Password</button>
+    )
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+
+newInput = () => console.log('Entering password...')
+
+render(){
+
+    return(
+      <div><input type="password" onKeyUp = {this.newInput}/></div>
+    )
+  }
+}
+
+export default Keypad;
